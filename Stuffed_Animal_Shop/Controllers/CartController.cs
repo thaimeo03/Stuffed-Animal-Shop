7f0b97e713ea0715065640b1ae90fb0d4f87e1a1
Thaimeo03 <thaitran15072003@gmail.com>
2023-10-09 17:21:48 +0700

    Fix layout

--- Stuffed_Animal_Shop/Controllers/CartController.cs
+++ Stuffed_Animal_Shop/Controllers/CartController.cs
@@ -1,13 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Stuffed_Animal_Shop.Data;
 using Stuffed_Animal_Shop.Models;
+using Stuffed_Animal_Shop.Services;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace Stuffed_Animal_Shop.Controllers
 {
+    [Authorize(Roles = "Admin, User")]
     public class CartController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        private readonly UserService _userService;
+
+        public CartController(ApplicationDbContext context)
+        {
+            _context = context;
+            _userService = new UserService(context);
+        }
         public IActionResult Index()
         {
+            var userEmail = HttpContext.User.FindFirst(ClaimTypes.Email)?.Value;
+
+            var user = this._userService.GetUserByEmail(userEmail);
+
+            if (user != null)
+            {
+                ViewBag.User = user;
+            }
             return View();
         }
 
