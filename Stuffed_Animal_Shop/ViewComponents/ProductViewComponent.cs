a508f9cda46f130ffddfba8afdc2674a1c70a63a
unknown <magcrusher0409@gmail.com>
2023-10-16 15:49:24 +0700

    Index with Products

---
+++ Stuffed_Animal_Shop/ViewComponents/ProductViewComponent.cs
@@ -0,0 +1,33 @@
+﻿namespace Stuffed_Animal_Shop.ViewComponents
+{
+	using Microsoft.AspNetCore.Mvc;
+	using Stuffed_Animal_Shop.Data;
+	using Stuffed_Animal_Shop.Models;
+	using Stuffed_Animal_Shop.Services;
+
+	public class ProductViewComponent : ViewComponent
+	{
+		private readonly ApplicationDbContext _context;
+
+		public ProductViewComponent(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public IViewComponentResult Invoke(bool isRanked)
+		{
+			if (isRanked)
+			{
+				List<Product> productsRanked = _context.Products.OrderBy(m => m.Sold).Take(8).ToList();
+
+				return this.View("ProductsByRanking", productsRanked);
+			}	
+			else
+			{
+				List<Product> productsArrived = _context.Products.OrderBy(m => m.CreatedAt).Take(8).ToList();
+
+				return this.View("ProductsByTimeArrived", productsArrived);
+			}
+		}
+	}
+}
