0c7e7173aae2c47917793c4c7023601271f83e30
unknown <magcrusher0409@gmail.com>
2023-10-18 15:56:09 +0700

    Overview tu Hieu

--- Stuffed_Animal_Shop/Services/EmailService.cs
+++ Stuffed_Animal_Shop/Services/EmailService.cs
@@ -3,37 +3,37 @@ using MimeKit;
 
 namespace Stuffed_Animal_Shop.Services
 {
-    public class EmailService
-    {
-        public void SendEmail(string to, string subject, string password)
-        {
-            var message = new MimeMessage();
-            message.From.Add(new MailboxAddress("Thai meo", "thaitran15072003!=@gmail.com"));
-            message.To.Add(new MailboxAddress("", to));
-            message.Subject = subject;
+	public class EmailService
+	{
+		public void SendEmail(string to, string subject, string password)
+		{
+			var message = new MimeMessage();
+			message.From.Add(new MailboxAddress("Thai meo", "thaitran15072003!=@gmail.com"));
+			message.To.Add(new MailboxAddress("", to));
+			message.Subject = subject;
 
-            var bodyBuilder = new BodyBuilder();
+			var bodyBuilder = new BodyBuilder();
 
 			// Read HTML content from file
 			string htmlBody = File.ReadAllText("Views/Shared/_EmailTemplate.cshtml");
 
 			bodyBuilder.HtmlBody = string.Format(htmlBody, password);
 
-            message.Body = bodyBuilder.ToMessageBody();
+			message.Body = bodyBuilder.ToMessageBody();
 
-            using (var client = new SmtpClient())
-            {
-                client.Connect("smtp.gmail.com", 587, false);
+			using (var client = new SmtpClient())
+			{
+				client.Connect("smtp.gmail.com", 587, false);
 
-                // Note: Since we don't have an OAuth2 token, disable the XOAUTH2 authentication mechanism.
-                client.AuthenticationMechanisms.Remove("XOAUTH2");
+				// Note: Since we don't have an OAuth2 token, disable the XOAUTH2 authentication mechanism.
+				client.AuthenticationMechanisms.Remove("XOAUTH2");
 
-                // Note: only needed if the SMTP server requires authentication
-                client.Authenticate("thaitran15072003@gmail.com", "sawu pcas bbbg kvjc");
+				// Note: only needed if the SMTP server requires authentication
+				client.Authenticate("thaitran15072003@gmail.com", "sawu pcas bbbg kvjc");
 
-                client.Send(message);
-                client.Disconnect(true);
-            }
-        }
-    }
+				client.Send(message);
+				client.Disconnect(true);
+			}
+		}
+	}
 }
