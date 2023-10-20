6552cf4e7ed21f283aacdde932e8c959657df810
Son <sonlearn155@gmail.com>
2023-10-20 10:36:47 +0700

    Review and filter, sort

---
+++ Stuffed_Animal_Shop/ViewModels/Reviews/CreateReview.cs
@@ -0,0 +1,19 @@
+﻿using Stuffed_Animal_Shop.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Stuffed_Animal_Shop.ViewModels.Reviews
+{
+    public class CreateReview
+    {
+
+        public string EmailUser { get; set; }
+
+        public int? Rating { get; set; }
+
+        public string? Comment { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
